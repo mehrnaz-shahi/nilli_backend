43d9aee2da219046f6106f9cf93cc46f71a0d57a
Mehrnaz shahi <91686230+mehrnaz-shahi@users.noreply.github.com>
2024-12-06 20:10:58 +0330

    edit db config

--- bakery_backend/settings.py
+++ bakery_backend/settings.py
@@ -101,11 +101,25 @@ WSGI_APPLICATION = 'bakery_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASES = {  
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nillipastry_nillipastry',
+        'USER': 'nillipastry_nillipastry',
+        'PASSWORD': 'wAGdwhIH_#)J',
+        'HOST': '127.0.0.1',  
+        'PORT': '3306',  
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }  
+    }  
 }
 
 # Password validation
@@ -227,7 +241,7 @@ LOGIN_REDIRECT_URL = '/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Or configure SMTP for production
-EMAIL_HOST = 'smtp.gmail.com'  # Example for Gmail
+EMAIL_HOST = 'mhrnzshahi.gmail.com'  # Example for Gmail
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'mhrnzshahi@gmail.com'
